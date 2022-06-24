b44f5d9c9cb7a6b737eeb2b362492f956a92461d
Rolexero <rotimiolaolu@gmail.com>
2022-06-24 19:00:25 +0100

    feat: :art: work on adding redux to the project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Counter from './components/Counter';
+import { Fragment } from 'react';
+import Header from './components/Header'
+import Auth from './components/Auth'
+import { useSelector } from "react-redux";
+import UserProfile from './components/UserProfile'
+
+
+function App() {
+    const auth = useSelector((state) => state.auth.isAuthenticated);
+
+  return (
+    <Fragment>
+      <Header />
+     {auth ? <UserProfile /> : <Auth />}
+      <Counter />
+    </Fragment>
+  );
+}
+
+export default App;
